6d26526bd543b11e53b4a600c7e81f9644471038
Doodooms <mailhebiauphoebus@gmail.com>
2025-11-27 21:41:07 +0100

    merci deepseek lol, 0.013s au lieu de 0.6 sec mais 1 couleur de plus qu'avec le mien

--- AlgorithmeColoriage.py
+++ AlgorithmeColoriage.py
@@ -1,6 +1,7 @@
 from abc import abstractmethod, ABC
 from Noeud import Noeud
 import numpy as np
+import time, datetime
 
 class AlgorithmeColoriage(ABC):
     """
@@ -33,73 +34,70 @@ class DSATUR(AlgorithmeColoriage):
     def trouver_coloriage(
             self, 
             partition: dict[str, set[Noeud]],
-            voisins : dict[Noeud, set[Noeud]]
+            voisins: dict[Noeud, set[Noeud]]
             ) -> dict[int, set[str]]:
         """
         :param partition: dico avec en clé la valeur du critere et la liste des noeuds ayant ce critere en valeurs
-        :param voisins: dico avec chaque noeud en clé et l'ensemble des voisins du noeud en valuer
-        :return: renvoie un dico avec la couleur en clé et la liste des criteres à colorier avec cette couleur.
+        :param voisins: dico avec chaque noeud en clé et l'ensemble des voisins du noeud en valeur
+        :return: renvoie un dico avec la couleur en clé et l'ensemble des criteres à colorier avec cette couleur.
         """
-        # Initialisation
+        start = time.time()
         coloriage = {}
-        dsat = {noeud : 0 for noeud in voisins.keys()}
+        dsat = {noeud: 0 for noeud in voisins.keys()}
         non_colorie = set(voisins.keys())
-        # voisins_partition associe à chaque critère l'ensemble des voisins des noeuds ayant ce critere
-        # ca nous permettra de connaitre les voisins du critere du noeud choisi
-        voisins_partition = {
-            critere: set.union(
-                *[voisins[noeud] for noeud in noeuds], set()
-            )
-            for critere, noeuds in partition.items()
-        }
+        degre = {noeud: len(voisins[noeud]) for noeud in voisins.keys()}
+        
+        # Précalcul des voisins par critère
+        voisins_partition = {}
+        for critere, noeuds in partition.items():
+            voisins_critere = set()
+            for noeud in noeuds:
+                voisins_critere.update(voisins[noeud])
+            voisins_partition[critere] = voisins_critere
+
+        # Dictionnaire pour retrouver le critère d'un nœud
+        noeud_to_critere = {}
+        for critere, noeuds in partition.items():
+            for noeud in noeuds:
+                noeud_to_critere[noeud] = critere
+
+        # Couleurs interdites par critère
+        couleurs_interdites = {critere: set() for critere in partition.keys()}
 
         while non_colorie:
-            # On choisit le noeud avec le DSAT maximum et degré max en cas d'égalité
-            max_dsat = -1
-            noeud_choisi = None
-            for node in non_colorie:
-                if (dsat[node] > max_dsat 
-                or (dsat[node] == max_dsat 
-                    and len(voisins[node]) > len(voisins[noeud_choisi or node]))
-                ):
-                    max_dsat = dsat[node]
-                    noeud_choisi = node
-            
-            # On trouve le critere du noeud choisi
-            critere_choisi = next(critere for critere, noeuds in partition.items() if noeud_choisi in noeuds)
-            
-            # On trouve la plus petite couleur disponible
-            couleur = None
-            for color in sorted(coloriage.keys()):
-                conflit = False
-                # On vérifie si critere_choisi peut avoir cette couleur, cad si la couleur est utilisée par un voisin
-                # dans voisins_partitions[critere_choisi], car on peut avoir une meme couleur pour 2 criteres différents
-                # si leurs partitions respectives sont "éloignées"
-                for voisin in voisins_partition[critere_choisi]:
-                    voisin_critere = next((crit for crit, set_noeuds in partition.items() if voisin in set_noeuds), None)
-                    if voisin_critere in coloriage[color]:
-                        conflit = True
-                        break
-                if not conflit:
-                    couleur = color
+            # Sélection du nœud avec DSAT max (degré max en cas d'égalité)
+            noeud_choisi = max(non_colorie, 
+                              key=lambda n: (dsat[n], degre[n]))
+            critere_choisi = noeud_to_critere[noeud_choisi]
+
+            # Trouver la plus petite couleur disponible pour le critère
+            couleur = 1
+            while True:
+                if couleur not in couleurs_interdites[critere_choisi]:
                     break
-            
-            # Si on trouve pas de couleurs possible on en créé une nouvelle
-            if couleur is None:
-                couleur = max(coloriage.keys()) + 1 if coloriage else 1
+                couleur += 1
+
+            # Ajouter le critère à la couleur
+            if couleur not in coloriage:
                 coloriage[couleur] = set()
-            
-            # On ajoute le critère à cette couleur (et donc la partition associée au final)
             coloriage[couleur].add(critere_choisi)
-            
-            # On retire tous les noeuds de ce critère de non_colorie (car ils sont mtn coloriés lol)
-            non_colorie.difference_update(partition[critere_choisi])
-            
-            # On met à jour les DSAT des noeuds étant dans non coloriés
+
+            # Mettre à jour les couleurs interdites pour les critères voisins
             for voisin in voisins_partition[critere_choisi]:
+                critere_voisin = noeud_to_critere[voisin]
+                couleurs_interdites[critere_voisin].add(couleur)
+
+            # Mettre à jour les DSAT pour les nœuds voisins non coloriés
+            for voisin in voisins[noeud_choisi]:
                 if voisin in non_colorie:
-                    dsat[voisin] += 1
-        
+                    critere_v = noeud_to_critere[voisin]
+                    dsat[voisin] = len(couleurs_interdites[critere_v])
+
+            # Retirer tous les nœuds du critère colorié
+            non_colorie.difference_update(partition[critere_choisi])
+
+        end = time.time()
+        print(f"Temps d'exécution DSATUR: {end - start:.4f} secondes")
         return coloriage
     
 if __name__=="__main__":
