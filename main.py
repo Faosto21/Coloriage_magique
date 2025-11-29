4c0e713c3daa4e730b48180d743b5b3e5c1d2451
Doodooms <mailhebiauphoebus@gmail.com>
2025-11-29 10:37:02 +0100

    tout propre et fonctionnel

--- main.py
+++ main.py
@@ -1,40 +1,39 @@
 from datetime import datetime, timedelta
+import tkinter as tk
 import pandas as pd
-from Noeud import Noeud
 
+from core.Noeud import Noeud
+from core.DiagrammeGant import DiagrammeGant
+from operators.AlgorithmeColoriage import DSATUR
 
-data = pd.read_csv("ressources/Planification.txt", sep=";")
-machines = pd.read_csv("ressources/Machine.txt")
-mapping_machines = {machines["centre"][i]: i for i in range(len(machines))}
-
-liste_noeuds = [
-    Noeud(
-        i,
-        mapping_machines[ope["centre"]],
-        ope["centre"],
-        ope["codprod"],
-        ope["codof"],
-        ope["sequence"],
-        ope["codop"],
-        datetime.fromisoformat(ope["dtedeb"]),
-        datetime.fromisoformat(ope["dtefin"]),
+if __name__ == "__main__":
+    # Initialisation des données
+    data = pd.read_csv("ressources/Planification.txt", sep=";")
+    machines = pd.read_csv("ressources/Machine.txt")
+    mapping_machines = {machines["centre"][i]: i for i in range(len(machines))}
+    liste_noeuds = [
+        Noeud(
+            i,
+            mapping_machines[ope["centre"]],
+            ope["centre"],
+            ope["codprod"],
+            ope["codof"],
+            ope["sequence"],
+            ope["codop"],
+            datetime.fromisoformat(ope["dtedeb"]),
+            datetime.fromisoformat(ope["dtefin"]),
+        )
+        for i, ope in data.iterrows()
+    ]
+    
+    # Initialisation des objets
+    root = tk.Tk()
+    root.title("Diagramme de Gant")
+    algo = DSATUR()
+    diagramme = DiagrammeGant(
+        root, liste_noeuds, mapping_machines, algo, max_time_gap=timedelta(days=7)
     )
-    for i, ope in data.iterrows()
-]
+    diagramme.pack(fill="both", expand=True)
 
-
-partition = Noeud.partition(
-    liste_noeuds
-)  # Partition de la liste de noeud selon le critère par défaut codeof
-voisins = Noeud.voisins_noeud(liste_noeuds)  # Dictionnaire des voisins des noeuds
-voisins_partition = {
-    critere: set.union(
-        *[voisins[noeud] for noeud in noeuds]
-    )  # Dictionnaires des voisins de chaque partie de partition
-    for critere, noeuds in partition.items()
-}
-print(len(partition))  # 108 valeurs différentes de codeof
-
-print(
-    voisins_partition["OF00000001"]
-)  # Union des voisins de l'ensemble des Noeuds ayant le codeof = OF00000001
+    # Lancement du script
+    root.mainloop()
